138684442f8b0f1ce48c8e9b1f2d902660886838
tectiv3 <git@getpanda.me>
2026-03-04 17:17:30 +0000

    fix: correct mlx-swift-lm version to 2.30.6 for MLXEmbedders support

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.12
 import PackageDescription
 
 let package = Package(
     name: "Notty",
     platforms: [.macOS(.v14)],
     dependencies: [
-        .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.21.0"),
-        .package(url: "https://github.com/ml-explore/mlx-swift-lm", from: "0.2.0"),
+        .package(url: "https://github.com/ml-explore/mlx-swift", .upToNextMinor(from: "0.30.6")),
+        .package(url: "https://github.com/ml-explore/mlx-swift-lm", .upToNextMinor(from: "2.30.6")),
         .package(url: "https://github.com/sindresorhus/KeyboardShortcuts", from: "2.0.0"),
     ],
     targets: [
@@ -15,7 +15,6 @@ let package = Package(
             dependencies: [
                 .product(name: "MLX", package: "mlx-swift"),
                 .product(name: "MLXNN", package: "mlx-swift"),
-                .product(name: "MLXRandom", package: "mlx-swift"),
                 .product(name: "MLXLLM", package: "mlx-swift-lm"),
                 .product(name: "MLXLMCommon", package: "mlx-swift-lm"),
                 .product(name: "MLXEmbedders", package: "mlx-swift-lm"),
