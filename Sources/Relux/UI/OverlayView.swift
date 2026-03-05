5ac9e8329c8b1637338829c0b77d642aea458bc6
tectiv3 <git@getpanda.me>
2026-03-05 21:21:35 +0000

    feat: route translate panel mode to TranslateView

--- Sources/Relux/UI/OverlayView.swift
+++ Sources/Relux/UI/OverlayView.swift
@@ -10,7 +10,7 @@ struct PanelRootView: View {
         case .clipboard:
             ClipboardHistoryView()
         case .translate:
-            EmptyView()
+            TranslateView()
         }
     }
 }
